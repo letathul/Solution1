ec9d6ff4122342b20a7b4dc4857449ea3f28cf96
Athul Babu <Athul Babu@INTELPC>
2024-04-23 20:28:30 -0400

    Add project files.

---
+++ ConsoleApp1/GenClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GenClass<T> where T : struct
+    {
+
+        private List<T> list;
+
+
+        public GenClass() {
+            list = new List<T>();
+
+        }
+
+        public void Add(T item) { 
+        
+            list.Add(item);
+        }
+
+        public T? getItem(int index) {
+
+            if (index < 0 || index >= list.Count) throw new IndexOutOfRangeException("Item not found");
+
+      
+            return list[index];
+        
+        }
+
+        public List<T> GetSortedList()
+        {
+            List<T> sortedList = new List<T>(list);
+            sortedList.Sort();
+            sortedList.Reverse();
+            return sortedList;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            GenClass<int> l = new GenClass<int>();
+            l.Add(1);
+            l.Add(5);   
+            l.Add(2);   
+        l.Add(3);
+            l.Add(7);   
+
+             
+
+
+            foreach (int i in l.GetSortedList())
+            {
+                Console.WriteLine($"{i}");  
+
+            }
+
+        }
+    }
+}
